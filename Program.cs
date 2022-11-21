0c09a51d5668b34d941a148d48ee2cc608347c1f
Makar <kondratiev.makar@yandex.ru>
2022-11-21 16:08:46 +0400

    open publicly swagger client in production builds

--- Program.cs
+++ Program.cs
@@ -53,11 +53,8 @@ builder.Services.AddSwaggerGen(opts =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
